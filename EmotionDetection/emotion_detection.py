edbb8b8940ed7514ce446ffaf8cc5eb176535336
vijayendarsinghs@gmail.com <vijayendarsinghs@gmail.com>
2026-03-31 02:38:10 -0400

    updates

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,7 +5,6 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     if len(text_to_analyze)==0 :
-        print("inloop")
         result = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None}
     else:
         response = requests.post(url, json = myobj, headers=headers)
--- server.py
+++ server.py
@@ -19,3 +19,4 @@ def render_index_page():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003)
+    
