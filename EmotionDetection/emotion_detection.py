12f7d5ed4a40f892504cb2bfa1d81ebe00164d14
vijayendarsinghs@gmail.com <vijayendarsinghs@gmail.com>
2026-03-31 02:34:31 -0400

    updates

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,11 +4,12 @@ def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    if "emotionPredictions" not in formatted_response:
-        result = "None"
+    if len(text_to_analyze)==0 :
+        print("inloop")
+        result = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None}
     else:
-        result = formatted_response['emotionPredictions'][0]['emotion']
-
+        response = requests.post(url, json = myobj, headers=headers)
+        formatted_response = json.loads(response.text)
+        result = formatted_response['emotionPredictions'][0]['emotion']      
+    print("result in emotion detection  : ", result)
     return result
